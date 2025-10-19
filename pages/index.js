c0523d0b28be0c121ad11862152c9e21bc881166
BrunoBrito375 <bb4102659@gmail.com>
2025-10-19 10:33:26 +0000

    adiciona scripits `lint:check` e `lint:fix`.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home() {
-    return <h1> Amanhã tem jogo do flamengo!!!</h1>
+  return <h1> Amanhã tem jogo do flamengo!!!</h1>;
+}
+
+function teste() {
+  console.log("indentação errada");
 }
 
 export default Home;
-
