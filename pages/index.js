6b1dca09813f23fcee3f1c7fd8324d8fa8348a90
BrunoBrito375 <bb4102659@gmail.com>
2025-10-23 00:34:38 +0000

    nova frase

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 function Home() {
-  return <h1> Amanhã tem jogo do flamengo!!!</h1>;
-}
-
-function teste() {
-  console.log("indentação errada");
+  return <h1> Hoje é feriadoooo?</h1>;
 }
 
 export default Home;
